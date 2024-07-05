5e2c0bfcb76438e738fcb0460f85e8ff792ae4ef
Juan Carlos Vilcherrez <juanc.vilcherrez@gmail.com>
2024-07-05 12:23:09 -0300

    axios api

--- api-frontend/src/app/page.jsx
+++ api-frontend/src/app/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import { Button, Form } from "react-bootstrap";
 import useAxios from "../hooks/useAxios"
@@ -11,48 +11,64 @@ export default function Home() {
 
   const [address, setAddress] = useState([]);
   const [request, setRequest] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const handleChange = (event) => {
-    setRequest(event.target.value);
-  };
   const sendRequest = (event) => {
     event.preventDefault();
     setAddress([]);
+    setErrorMessage("");
     fetchData({
       url: `/api/normalizar/?direccion=${request}`,
       method: 'GET'
     });
-    console.log(response)
-    setAddress(response.direccionesNormalizadas)
   };
 
-  return (
-    <main className={styles.main}>
-      <Form onSubmit={sendRequest} noValidate className="">
-        <Form.Group className="">
-          <Form.Label>Ingresar Direccion</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Calle y/o numero"
-            name="direccion"
-            value={request}
-            onChange={handleChange}
-          />
-        </Form.Group>
-        <Button variant="dark" type="submit" >
-          Registrar
-        </Button>
-      </Form>
+  useEffect(() => {
+    if (response && response.direccionesNormalizadas) {
+      setAddress(response.direccionesNormalizadas);
+    }
+    if (error) {
+      setErrorMessage(error);
+    }
+  }, [response, error]);
 
-      <div>
-        {
-          address.map((ad, index) =>
-            <div key={index}>
-              <p> Direccion: {ad.direccion} </p>
-              <p>Calle: {ad.nombre_calle} </p>
-            </div>
-          )
-        }
+  const handleChange = (event) => {
+    setRequest(event.target.value);
+  };
+
+
+  return (
+    <main className={` d-flex justify-content-center align-items-center`}>
+      <div className={`${styles.main}`}>
+        <h2 className="col-12 fw-bold">Direcciones Sugeridas</h2>
+        <Form onSubmit={sendRequest} noValidate className={styles.form}>
+          <Form.Group className="">
+            <Form.Label>Ingresar Direccio</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Calle y/o numero"
+              name="direccion"
+              value={request}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Button variant="warning" type="submit" className="mt-3 " >
+            Buscar
+          </Button>
+        </Form>
+        {/* {address.length > 0 && <p className="p-3">Resultados:</p>} */}
+        {errorMessage && <p className=" alert alert-danger text-center">{errorMessage} </p>}
+        <div className={`${styles.responses} `}>
+          {
+            address.map((ad, index) =>
+              <div key={index} className="p-2" >
+                <p> <span class="fw-bold">Dirección:</span> {ad.direccion} </p>
+                <p> <span class="fw-bold">Calle:</span> {ad.nombre_calle} </p>
+              </div>
+            )
+          }
+        </div>
+
       </div>
     </main>
   );
